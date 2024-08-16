249256e7a447b076c0744ae031d25287517501e1
Kun Zhao <kunzhaoz@gmail.com>
2024-08-16 14:49:02 -0400

    Remove unused code

--- src/bot.py
+++ src/bot.py
@@ -1,8 +1,12 @@
 import discord, os
 from dotenv import load_dotenv
 from discord.ext import tasks
-from market_recipe import *
+from item import *
+from message import Item as ItemMessage
+from message import Craftable as CraftableMessage
 
+item_message = ItemMessage()
+craftable_message = CraftableMessage()
 
 channel_id_test = 715392947608354886
 
@@ -36,54 +40,32 @@ def run_discord_bot():
 
         if message.author == client.user:
             return
-        """
-        if '!m' in user_message:
-            name, info = market_info(user_message[3:])
-            embed = discord.Embed(colour=discord.Colour.red(),
-                                  description=info,
-                                  title=f'{name} (Market Info)')
-            
-            await message.channel.send(embed=embed)
-
-        elif '!r' in user_message:
-            verbose = True if '!v' in user_message.lower() else False
-            mastery = 1000
-            clean_input = user_message.replace('!r', '').replace('!v', '').strip()
-            if user_message.split()[-1].isdigit():
-                mastery = int(clean_input.split()[-1])
-                name, info = recipe_info(clean_input, mastery, verbose)
-            else:
-                name, info = recipe_info(clean_input, mastery, verbose)
-
-            embed = discord.Embed(colour=discord.Colour.red(),
-                                  description=info,
-                                  title=f"{name} (Recipe Info)")
-            await message.channel.send(embed=embed)
-        """
 
         if "!m" in user_message:
-            input = user_message.replace('!t1', '').strip()
-            info = Market(input)
-            name = f"{info.enhancement_level.upper()}: {info.name}" if info.enhancement_level else info.name
+            input = user_message.replace('!m', '').strip()
+            info = Item(input)
+            deliverable = item_message.deliverable(info)
+            header = f"{info.enhancement_level.upper()}: {info.name}" if info.enhancement_level else info.name
             embed = discord.Embed(colour=discord.Colour.red(),
-                                  description=info.deliverable(),
-                                  title=f"{name} (Market Info)")
+                                  description=deliverable,
+                                  title=f"{header} (Market Info)")
             
             await message.channel.send(embed=embed)
 
         elif "!r" in user_message:
             verbose = True if '!v' in user_message.lower() else False
             mastery = 1000
-            clean_input = user_message.replace('!t2', '').replace('!v', '').strip()
+            clean_input = user_message.replace('!r', '').replace('!v', '').strip()
             if clean_input.split(" ")[-1].isdigit():
                 mastery = int(clean_input.split(" ")[-1])
                 clean_input = ' '.join(clean_input.split(" ")[:-1])
 
             info = Craftable(clean_input, mastery, verbose)
-            name = f"{info.enhancement_level.upper()}: {info.name}" if info.enhancement_level else info.name
+            deliverable = craftable_message.deliverable(info)
+            header = f"{info.enhancement_level.upper()}: {info.name}" if info.enhancement_level else info.name
             embed = discord.Embed(colour=discord.Colour.red(),
-                                  description=info.deliverable(),
-                                  title=f"{name} (Recipe Info)")
+                                  description=deliverable,
+                                  title=f"{header} (Recipe Info)")
             
             await message.channel.send(embed=embed)
 
